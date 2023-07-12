3ef624fadc5e45b26c036dfcb6b0fd821e7fe4db
Scott Arthur <scott@scottatron.com>
2023-07-12 13:01:54 +0000

    fetch the build info

--- netlify/functions/redirect.js
+++ netlify/functions/redirect.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
-const path = require('path')
+const fetch = require('node-fetch');
 
 exports.handler = async function(event, context) {
-  const buildInfo = fs.readFileSync(path.join(__dirname, '../public/.well-known/build.json'), 'utf-8')
-  const {version} = JSON.parse(buildInfo)
+  const response = await fetch('https://www.scottatron.com/.well-known/build.json');
+  const { version } = await response.json();
+
   const url = `https://github.com/scottatron/scottatron.com/releases/download/${version}/Scott-Arthur-CV-${version}.pdf`
 
   return {
